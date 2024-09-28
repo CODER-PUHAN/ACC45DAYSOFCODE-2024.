516fb352d891a3d0b288f4f0037144d12ffdf1f5
CODER-PUHAN <siddharth.puhan15102006@gmail.com>
2024-09-28 21:51:43 +0530

    Add files via upload

---
+++ Day 1 - CWC23QUALIF.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int x;
+    
+    scanf("%d",&x);
+    if(x>=12){
+        printf("yes");
+    }
+    else{
+        printf("no");
+    }
+	// your code goes here
+
+}
+
