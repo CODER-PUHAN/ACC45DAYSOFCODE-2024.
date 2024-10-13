cef4d5ef4728a6404037ae9c703af11f5c86d69b
CODER-PUHAN <siddharth.puhan15102006@gmail.com>
2024-10-13 17:01:45 +0530

    Add files via upload

---
+++ DAY16 - NOTEBOOK.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--){
+	    int n;
+	    scanf("%d",&n);
+	    int x= (n*1000)/100;
+	    printf("%d\n",x);
+
+	}
+
+}
+
